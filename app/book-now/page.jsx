7d8f6dc0854c2b7cdbf194e8408241b2dc9e7d2d
Bhumi Bhatt <shinedorrie.bookish55@gmail.com>
2024-07-29 15:16:55 +0530

    Text Box Centered

--- app/book-now/page.jsx
+++ app/book-now/page.jsx
@@ -6,22 +6,24 @@ const EngageWithUs = () => {
     <>
       <div className="w-full px-5 lg:px-10 py-10">
         <TransitionEffect />
-        <div className="flex flex-col items-center justify-center gap-3">
+        <div className="flex flex-col items-center justify-center gap-3 ml-auto pr-20 text-center w-full">
           <h3>Book Hours</h3>
           {/* <p>Let&apos;s have a chat</p> */}
         </div>
         <div className="container mx-auto">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-8">
-            <form className="flex flex-col gap-6">
-              <div className="flex flex-col gap-3">
-                <label htmlFor="name">Add Amount</label>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-8 ">
+            <form className="flex flex-col gap-6 items-center ml-auto pr-20 ml-auto pr-8 absolute left-1/2 transform -translate-x-1/2">
+              <div className="flex flex-col gap-3 items-center w-full">
+                <label htmlFor="name" className="text-center ml-auto pr-20 absolute left-1/2 transform -translate-x-1/2 ">Add Amount</label>
                 <input
                   type="text"
                   id="name"
                   name="name"
-                  className="border border-secondary ring-current bg-transparent rounded-lg p-3"
+                  placeholder="Enter amount here"
+                  className="border p-2 w-full mt-8 border-secondary ring-current bg-transparent rounded-lg p-3 w-full ml-auto pr-20"
                 />
               </div>
+              
               {/* <div className="grid grid-cols-2 gap-2">
                 <div className="flex flex-col gap-3">
                   <label htmlFor="email">Email</label>
@@ -52,7 +54,7 @@ const EngageWithUs = () => {
                   className="border border-secondary  bg-transparent rounded-lg p-3 h-32  resize-none "
                 />
               </div> */}
-              <div>
+              <div className="w-full flex justify-center">
                 <button className="bg-secondary py-3 px-6 text-background rounded-lg border border-secondary hover:text-heading duration-300 ease-out hover:bg-background">
                   Buy Now
                 </button>
